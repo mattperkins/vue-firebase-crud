6f6c216a4e9c59750e3da24eeb719f7cf9ca8e61
mattperkins <github@mattperkins.com>
2018-01-21 00:24:33 +0000

    add full crud functionality

--- src/App/app.js
+++ src/App/app.js
@@ -1,8 +1,39 @@
+// firebase > database > rules > "auth != null"
+import { namesRef } from '../firebase'
+
 export default {
   data () {
     return {
-      label: 'Name:',
-      name: 'Fred'
+      name: '',
+      placeholder: 'Add a person here'
     }
+  },
+  firebase: {
+      names: namesRef
+  },
+  methods: {
+      submitName(){
+          if(!this.name == '')
+          namesRef.push({
+              name: this.name, edit: false
+          }), this.name = ''
+          else alert('Please add a name')
+      },
+      removePerson(key){
+          namesRef.child(key).remove()
+      },
+      editPerson(key){
+          namesRef.child(key).update( {edit: true} )
+      },
+      cancelEdit(key){
+          namesRef.child(key).update( {edit: false} )
+      },
+      saveEdit(person){
+          const key = person['.key']
+          namesRef.child(key).set( {name: person.name, edit: false} )
+      }
   }
-}
+} 
+
+
+
